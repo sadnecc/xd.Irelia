f3c92196ec4fa69f5a8efb91a6517eb959023027
ensoulxd <ensoulxd@outlook.com>
2020-08-07 19:00:45 +0300

    10.16

---
+++ README.md
@@ -0,0 +1,1 @@
+<a href="https://ibb.co/2g37V1v"><img src="https://i.ibb.co/4PRZvn8/irelia.jpg" alt="irelia" border="0"></a>
---
+++ xd.Irelia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Permissions;
+using EnsoulSharp.SDK;
+using xd.Irelia.Properties;
+
+namespace xd.Irelia
+{
+    class Irelia
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+        [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
+        private static void OnGameLoad()
+        {
+            try
+            {
+                var a = Assembly.Load(Resources.Irelia);
+                var myType = a.GetType("xd.Irelia");
+                var methon = myType.GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+
+                if (methon != null)
+                {
+                    methon.Invoke(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
